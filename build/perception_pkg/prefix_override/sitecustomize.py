b01bd0ac60a9225558ab244689ede9fc4f8fef86
Ignacio Gajardo <94397851+slawhs@users.noreply.github.com>
2025-05-05 12:23:27 -0400

    Delete build directory

--- build/perception_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/grupo4/capstone_ws/install/perception_pkg'
--- build/pointcloud_to_laserscan/ament_cmake_symlink_install_targets_1_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/grupo4/capstone_ws/build/pointcloud_to_laserscan/pointcloud_to_laserscan_node" "TARGETS" "pointcloud_to_laserscan_node" "DESTINATION" "lib/pointcloud_to_laserscan")
