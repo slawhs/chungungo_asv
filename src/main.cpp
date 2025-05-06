70e8761954c5e3e68565cd2cad4610af3ae426e5
Gustavo Vidal <govidal@uc.cl>
2025-05-06 12:10:13 -0400

    control motores serial

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ String device_name = "ESP32-BT-Slave";
 #define mot1_enA 27
 #define mot1_in1 14
 #define mot1_in2 26
-#define enc1_ENCODER_A 22
-#define enc1_ENCODER_B 23
+#define enc1_ENCODER_A 23
+#define enc1_ENCODER_B 22
 
 //Motor 2
 // #define mot2_enA 32
@@ -21,11 +21,12 @@ String device_name = "ESP32-BT-Slave";
 
 #define PULSOS_POR_VUELTA 16
 
-volatile unsigned int enc1_pulsos = 0;
+volatile long enc1_pulsos = 0;
 unsigned long ultimaMedicion = 0;
 float mot1_rpm = 0;
 int mot1_dir = 1; // 1 si es sentido horario, -1 si es antihorario, 0 si es frenado
 int enc1_dir = 0;
+long oldposition0 = 0;
 
 String msg;
 
@@ -37,32 +38,31 @@ String msg;
 // Variables para PID del primer motor
 const float Kp = 1.0, Ki = 0.1, Kd = 0.1, Ts = 0.1;
 float c_prev = 0, e_prev1 = 0, e_prev2 = 0;
-float setpoint = 0;
+float setpoint = 255;
 
 void IRAM_ATTR contarPulsoA() {
     if (digitalRead(enc1_ENCODER_A) == digitalRead(enc1_ENCODER_B)) {
-        enc1_pulsos--;
-    } else {
         enc1_pulsos++;
+    } else {
+        enc1_pulsos--;
     }
 }
-
 void IRAM_ATTR contarPulsoB() {
     if (digitalRead(enc1_ENCODER_A) == digitalRead(enc1_ENCODER_B)) {
-        enc1_pulsos++;
-    } else {
         enc1_pulsos--;
+    } else {
+        enc1_pulsos++;
     }
 }
 
 void mot1_velocidad(int velocidad, int dir) {
     if (velocidad < 0) {
-        velocidad = 255;
-    } else if (velocidad > 255) {
         velocidad = 0;
+    } else if (velocidad > 255) {
+        velocidad = 255;
     }
-    SerialBT.printf("velocidad: %d \n", velocidad);
-    SerialBT.printf("dir: %d \n", dir);
+    // SerialBT.printf("velocidad: %d \n", velocidad);
+    // SerialBT.printf("dir: %d \n", dir);
     if (dir == 1){
         if (velocidad < 185) {
             ledcWrite(MOT1, velocidad);
@@ -102,7 +102,6 @@ void setup() {
     attachInterrupt(enc1_ENCODER_A, contarPulsoA, CHANGE);
     attachInterrupt(enc1_ENCODER_B, contarPulsoB, CHANGE);
 
-
     digitalWrite(mot1_in1, LOW);
     digitalWrite(mot1_in2, HIGH);
 
@@ -122,26 +121,28 @@ void loop() {
     delay(20);
     if (millis() - ultimaMedicion >= 100) {
         // --- mido rpm ---
-        mot1_rpm = (enc1_pulsos * 600.0) / PULSOS_POR_VUELTA;
-        enc1_pulsos = 0;
+        float rpm = 1000.0 * PULSOS_POR_VUELTA / 60.0; // RPM
+        mot1_rpm = (float)(enc1_pulsos - oldposition0) * rpm / (millis() - ultimaMedicion); //RPM
+        oldposition0 = enc1_pulsos;
         ultimaMedicion = millis();
 
         // --- PID discreto recursivo ---
-        float e0 = setpoint - mot1_rpm;
-        float a0 =  Kp + Ki*Ts + Kd/Ts;
-        float a1 = -Kp - 2.0*(Kd/Ts);
-        float a2 =  Kd/Ts;
-        float c0 = c_prev + a0*e0 + a1*e_prev1 + a2*e_prev2;
-        e_prev2 = e_prev1; e_prev1 = e0; c_prev = c0;
+        // float e0 = setpoint - mot1_rpm;
+        // float a0 =  Kp + Ki*Ts + Kd/Ts;
+        // float a1 = -Kp - 2.0*(Kd/Ts);
+        // float a2 =  Kd/Ts;
+        // float c0 = c_prev + a0*e0 + a1*e_prev1 + a2*e_prev2;
+        // e_prev2 = e_prev1; e_prev1 = e0; c_prev = c0;
 
-        if (c0 > 0) {
-            mot1_dir = -1; // sentido horario
-        } else if (c0 < 0) {
-            mot1_dir = 1; // sentido antihorario
+        int pwm = setpoint;
+        if (pwm > 0){
+            mot1_dir = 1; // sentido horario
+        } else if (pwm < 0){
+            mot1_dir = -1; // sentido antihorario
         } else {
             mot1_dir = 0; // frenado
         }
-        int pwm = abs((int)round(c0));
+        pwm = abs(pwm);
         mot1_velocidad(pwm, mot1_dir);
 
         // --- debug por BT ---
