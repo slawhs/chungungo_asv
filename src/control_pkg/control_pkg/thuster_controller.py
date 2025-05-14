0fb5e89597319d8aec5826e1f2426d745d9c7caf
slawhs <gajardo.ignacio@uc.cl>
2025-05-14 17:11:40 -0400

    WIP: thruster_controller

--- src/control_pkg/control_pkg/thuster_controller.py
+++ src/control_pkg/control_pkg/thuster_controller.py
@@ -1,17 +1,42 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
 from serial import Serial
 
-serial = Serial('/dev/tty', 115200)
+DEVICE_NAME = '/dev/ttyUSB0'
 
 
-vel1, dir1 = input("ingrese vel1,dir1").strip().split(",")
-vel2, dir2 = input("ingrese vel2,dir2").strip().split(",")
+class ThrusterController(Node):
+    def __init__(self):
+        super().__init__("ThrusterController")
 
-rpm_msg = f"{vel1},{vel2},{dir1},{vel2}"
+        # -------- Publishers and Subscribers --------
+        # self.left_motor_sub(String,)
+        # -------- Atributes --------
 
-serial.write(rpm_msg.encode())
+        # -------- Setup Routines --------
+        self.setup_serial()
+
+    def setup_serial(self):
+        
+        self.serial = Serial(DEVICE_NAME, baudrate=115200)
+        if not self.serial.isOpen():
+            self.get_logger().error('Serial port is closed')
+
+    
+
+
+vel1, dir1 = input("ingrese vel1,dir1: ").strip().split(",")
+vel2, dir2 = input("ingrese vel2,dir2: ").strip().split(",")
+
+rpm_msg = f"{vel1},{dir1},{vel2},{dir2}"
+serial.write(rpm_msg.encode('utf-8'))
 
 while True:
     rpm_feedback = serial.readline()
+    rpm_feedback = rpm_feedback.decode('utf-8')
 
     rpm_motor1, rpm_motor2 = rpm_feedback.strip().split(" | ")
 
