45572d85b4cff134557e4bc001d1d1b7cb2bd804
slawhs <gajardo.ignacio@uc.cl>
2025-06-16 16:24:28 -0400

    refactor: changed method to recieve feedback messages from ESP32

--- src/control_pkg/control_pkg/thrusters_driver.py
+++ src/control_pkg/control_pkg/thrusters_driver.py
@@ -8,7 +8,7 @@ from chungungo_interfaces.srv import VelocityCommand
 from serial import Serial
 import time
 
-DEVICE_NAME = '/ttyCH341USB0'
+DEVICE_NAME = '/ttyUSB0'
 BAUD_RATE = 115200
 
 class Thrusters(Node):
@@ -33,29 +33,26 @@ class Thrusters(Node):
 
     def vel_command_cb(self, request, response):
         vel_cmd = f"{request.left_velocity},{request.right_velocity}\n"
-        self.get_logger().info(f"""
-                               ==================== VELOCITY COMMAND RECIEVED ====================
-                               Left setpoint:{request.left_velocity} rpm
-                               Right setpoint:{request.right_velocity} rpm
-                               """)
-        # self.get_logger().info(f"Left setpoint:{request.left_velocity} rpm")
-        # self.get_logger().info(f"Right setpoint:{request.right_velocity} rpm")
-
+        self.get_logger().info(f"[Setpoints] Left: {request.left_velocity} RPM | Right: {request.right_velocity} RPM")
         self.serial.write(vel_cmd.encode("utf-8"))
         response.left_rpm, response.right_rpm = self.get_rpm_feedback()
 
         return response
 
     def get_rpm_feedback(self):
-        self.get_logger().info("Getting feedback...")
-        rpm_feedback = self.serial.readline().decode('ascii', errors='replace')
-        self.get_logger().info("Feedback recieved")
+        # self.get_logger().info("Getting feedback...")
+        self.serial.flushInput()
+        rpm_esp = self.serial.read_until(b'\r')
+        rpm_feedback = str(rpm_esp, encoding='utf-8')
+        # rpm_feedback = self.serial.readline().decode('utf-8', errors='replace')
+        self.get_logger().info(rpm_feedback)
+        # self.get_logger().info("Feedback recieved")
 
         left_rpm, right_rpm = rpm_feedback.strip().split("|")
         left_rpm = int(round(float(left_rpm)))
         right_rpm = int(round(float(right_rpm)))
 
-        self.get_logger().info(f"Left: {left_rpm} RPM | Right: {right_rpm} RPM")
+        self.get_logger().info(f"[Feedback] Left: {left_rpm} RPM | Right: {right_rpm} RPM")
 
         return left_rpm, right_rpm
 
