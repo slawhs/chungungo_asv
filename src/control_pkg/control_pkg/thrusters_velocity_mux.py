d75259e804795b141db472d9c66d34c5ad298ef8
Ignacio Gajardo <ignagaja@gmail.com>
2025-06-24 19:06:59 -0400

    refactor: added support for action_processing node

--- src/control_pkg/control_pkg/thrusters_velocity_mux.py
+++ src/control_pkg/control_pkg/thrusters_velocity_mux.py
@@ -5,7 +5,9 @@ from rclpy.node import Node
 
 from chungungo_interfaces.srv import VelocityCommand
 from chungungo_interfaces.msg import ThrustersVelocity
-from geometry_msgs.msg import Vector3  #? Do custom interface for joystick_speed
+from std_msgs.msg import String
+
+BASE_VEL = 50.0
 
 class ThrustersVelMux(Node):
     def __init__(self):
@@ -14,7 +16,8 @@ class ThrustersVelMux(Node):
         # -------- Publishers, Subscribers and Services --------
         #* crear subscriber de nodos de teleop, control de boyas, control de distancias. 
         self.joystick_vel_sub = self.create_subscription(ThrustersVelocity, '/joystick_velocity', self.joystick_vel_cb, 10)
-        self.buoy_distance_vel_sub = self.create_subscription()
+        self.buoy_distance_vel_sub = self.create_subscription(ThrustersVelocity, '/buoy_distance_velocity', self.buoy_distance_vel_cb, 10)
+        self.mode_sub = self.create_subscription(String, "/operation_mode", self.mode_cb, 10)
 
         self.vel_cmd_cli = self.create_client(VelocityCommand, '/vel_command')
         while not self.vel_cmd_cli.wait_for_service(timeout_sec=0.5):
@@ -28,16 +31,40 @@ class ThrustersVelMux(Node):
         self.joystick_left_vel = 0.0
         self.joystick_right_vel = 0.0
 
+        self.buoy_dist_left_vel = 0.0
+        self.buoy_dist_right_vel = 0.0 
+
+        self.mode = "standby"
+
         # -------- Setup Routines --------
+
+    def mode_cb(self, msg):
+        self.mode = msg.data
 
     def joystick_vel_cb(self, msg):
         self.joystick_left_vel = msg.left_velocity
         self.joystick_right_vel = msg.right_velocity
 
+
+    def buoy_distance_vel_cb(self, msg):
+        self.buoy_dist_left_vel = msg.left_velocity
+        self.buoy_dist_right_vel = msg.right_velocity
+
+
     def vel_mux_cb(self):
-        # Joystick velocity
-        self.request.left_velocity = self.joystick_left_vel
-        self.request.right_velocity = self.joystick_right_vel
+        if self.mode == "joystick":
+            # Joystick velocity
+            self.request.left_velocity = self.joystick_left_vel
+            self.request.right_velocity = self.joystick_right_vel
+    
+        elif self.mode == "buoy":
+            self.request.left_velocity = self.buoy_dist_left_vel
+            self.request.right_velocity = self.buoy_dist_right_vel
+
+        elif self.mode == "standby":
+            self.request.left_velocity = 0.0
+            self.request.right_velocity = 0.0
+
         self.future = self.vel_cmd_cli.call_async(self.request)
 
         if self.future.done():
@@ -48,7 +75,7 @@ class ThrustersVelMux(Node):
             else:
                 self.get_logger().info(f"RPM Left = {response.left_rpm}")
                 self.get_logger().info(f"RPM Right = {response.right_rpm}")
-   
+
 
 
 def main(args=None):
--- src/control_pkg/setup.py
+++ src/control_pkg/setup.py
@@ -22,7 +22,9 @@ setup(
         'console_scripts': [
             "joystick_velocity = control_pkg.joystick_velocity:main",
             "thrusters_driver = control_pkg.thrusters_driver:main",
-            "thrusters_velocity_mux = control_pkg.thrusters_velocity_mux:main"
+            "thrusters_velocity_mux = control_pkg.thrusters_velocity_mux:main",
+            "action_processing = control_pkg.action_processing:main",
+            "buoy_avoidance = control_pkg.buoy_avoidance:main"
         ],
     },
 )
