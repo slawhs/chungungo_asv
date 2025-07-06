3df25a63e7b2451a0302358fee994f7bdaef9085
Coke <jorge.parga@uc.cl>
2025-07-06 01:12:20 -0400

    feat: class Buoy moved to its own file.

---
+++ src/perception_pkg/perception_pkg/buoy.py
@@ -0,0 +1,41 @@
+class Buoy():
+    def __init__(self, id: int, color: str, limit: int, counter: int = 0):
+        self.id = id
+        self.color = color
+
+        self.limit = limit
+        self.limit_counter = counter
+
+        self.distance = None
+        self.angle = None
+
+        self.name = ""
+
+        self.set_name()
+
+    def __str__(self):
+        return f"{self.name}, Distance: {self.distance}, Angle: {self.angle}"
+
+    def get_id(self):
+        return self.id
+
+    def get_color(self):
+        return self.color
+    
+    def set_distance(self, distance):
+        self.distance = distance
+
+    def get_distance(self):
+        return self.distance
+    
+    def set_angle(self, angle):
+        self.angle = angle
+
+    def get_angle(self):
+        return self.angle
+    
+    def set_name(self):
+        if self.id % 2 == 0:
+            self.name = f"Buoy {self.id} - {self.color} - Left"
+        else:
+            self.name = f"Buoy {self.id} - {self.color} - Right"
